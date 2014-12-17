f9aca72ba4345541748e681805284d63c0339a13
Thomas Gärtner <mischka3112@web.de>
2014-12-17 15:48:48 +0100

    first testable version

--- astar.lua
+++ astar.lua
@@ -1,6 +1,7 @@
 local abs, max, min, pow, sin , huge = math.abs, math.max, math.min, math.pow, math.sin, math.huge;
 
 function courseplay:calcMoves(vehicle,fruitType) 
+	--print(string.format("calcMoves(vehicle, fruitType%s) ",tostring(fruitType)))
 	local combine = vehicle.cp.activeCombine
 	local sx,sy,sz =  getWorldTranslation(vehicle.cp.DirectionNode);
 	local tableCw = {}
@@ -9,14 +10,14 @@ function courseplay:calcMoves(vehicle,fruitType)
 	local travelDistanceCcw,travelDistanceCw = 0,0
 	local startDistance = courseplay:distanceToPoint(combine, sx,sy,sz)
 	local ldX, ldY,ldZ = courseplay:getDriveDirection(combine.cp.DirectionNode, sx,sy,sz)
-	local timeoutDistance = 1500--courseplay:distanceToPoint(combine, sx,sy,sz) * 2
+	local timeoutDistance = courseplay:distanceToPoint(combine, sx,sy,sz) * 2
 	local angleGrad = math.deg(Utils.getYRotationFromDirection(ldX, ldZ))
 	--print("angleGrad = "..tostring(angleGrad))
 	--print("call Ccw")
-	tableCcw, travelDistanceCcw = courseplay:scanFruit(combine,angleGrad,1,huge,timeoutDistance,startDistance)
+	tableCcw, travelDistanceCcw = courseplay:scanFruit(combine,angleGrad,1,fruitType,huge,timeoutDistance,startDistance)
 	print("travelDistanceCcw = "..tostring(travelDistanceCcw))
 	--print("call Cw")
-	tableCw , travelDistanceCw  = courseplay:scanFruit(combine,angleGrad,-1, travelDistanceCcw,timeoutDistance,startDistance)
+	tableCw , travelDistanceCw  = courseplay:scanFruit(combine,angleGrad,-1,fruitType, travelDistanceCcw,timeoutDistance,startDistance)
 	print("travelDistanceCw = "..tostring(travelDistanceCw))
 	if travelDistanceCw < travelDistanceCcw then
 		outputTable = tableCw
@@ -26,12 +27,13 @@ function courseplay:calcMoves(vehicle,fruitType)
 	return outputTable
 end
 
-function courseplay:scanFruit(combine,startAngle,direction,maxLastDistance,timeoutDistance,startDistance)
+function courseplay:scanFruit(combine,startAngle,direction,fruitType,maxLastDistance,timeoutDistance,startDistance)
 	local tempTable ={}
 	local distance = startDistance   
 	local totalDistance = 0
 	local foundFruit = false
 	local angleGrad = startAngle
+	local backUpAngle = 0
 	local cx,cy,cz =  getWorldTranslation(combine.cp.DirectionNode);
 	local i = 1
 	while i<= 360 do
@@ -42,33 +44,40 @@ function courseplay:scanFruit(combine,startAngle,direction,maxLastDistance,timeo
 		elseif angleGrad < 0 then 
 			angleGrad = angleGrad +360
 		end
-		--print(string.format("1st checking %s Grad distance: %s",tostring(angleGrad),tostring(distance)))
+		--print(string.format("1st checking %d Grad distance: %d, fruitType: %s",angleGrad,distance,tostring(fruitType)))
 		local ldX, ldZ = Utils.getDirectionFromYRotation(math.rad(angleGrad))
 		local wdX,wdY,wdZ = localDirectionToWorld(combine.cp.DirectionNode, ldX, 0,ldZ)
 		local lastDistance = distance
 		foundFruit, distance = courseplay:getFieldBorderInDirection(combine,wdX,wdZ,fruitType,distance,timeoutDistance)
 		if not foundFruit then -- backup if smart fining doesn't find st
+			--print(string.format("1st checking no fruit found"))
 			foundFruit, distance = courseplay:getFieldBorderInDirection(combine,wdX,wdZ,fruitType,0,timeoutDistance)
+			--print(string.format("1st checking no fruit found new distance: %s",tostring(distance)))
 			if not foundFruit then --still nothing found
+				--print(string.format("1st checking backup no fruit found -> break"))
 				break
 			end
+		else
+			timeoutDistance = distance * 3
+			--print(string.format("1st checking new distance: %s",tostring(distance)))
 		end
 		--is there a jump in distances?
-	
-		if lastDistance > distance+50 then
-			--print(string.format("lastDistance (%d) > distance+50(%d)	jump found at %s Grad", lastDistance ,distance,tostring(angleGrad)))
+									--50
+		if lastDistance > distance+30 and math.ceil(angleGrad) ~= math.ceil(backUpAngle) then
+			--print(string.format("lastDistance (%d) > distance+50(%d)	jump found at %d Grad", lastDistance ,distance,angleGrad))
 			local secondAngleGrad = angleGrad +(2*-direction)
 			--print(string.format("	set angle to %s Grad",tostring(secondAngleGrad)))
 			local goBack = true
 			local secondFoundFruit =false
 			local secondDistance = lastDistance
+			backUpAngle = angleGrad
 			while goBack do
 				i= i-1
-				--print(string.format("	2nd checking %s Grad",tostring(secondAngleGrad)))
+				--print(string.format("	2nd checking %d Grad, distance: %d, fruitType: %s",secondAngleGrad,secondDistance,tostring(fruitType)))
 				local sldX, sldZ = Utils.getDirectionFromYRotation(math.rad(secondAngleGrad))
 				local swdX,swdY,swdZ = localDirectionToWorld(combine.cp.DirectionNode, sldX, 0,sldZ)
-				--print(string.format("courseplay:getFieldBorderInDirection(combine,wdX,wdZ,fruitType,secondDistance(%s),10,true)",tostring(secondDistance)))
 				secondFoundFruit, distance = courseplay:getFieldBorderInDirection(combine,swdX,swdZ,fruitType,secondDistance,10,true)
+				
 				if not secondFoundFruit then
 					--print("	fruit is full, go back to normal")
 					angleGrad = secondAngleGrad + (2*direction)
@@ -76,6 +85,7 @@ function courseplay:scanFruit(combine,startAngle,direction,maxLastDistance,timeo
 					--print("distance = "..tostring(distance))
 					goBack = false
 				else
+					--print(string.format("	2nd checking new distance: %d",distance))
 					secondDistance = distance 
 									-- put new way point to table
 									local icx = cx+(swdX*(distance-5))
@@ -131,7 +141,7 @@ function courseplay:getFieldBorderInDirection(combine,wdX,wdZ,fruitType,distance
 	for i=safetyRadius,timeoutDistance,forward do
 		local x = cx+(wdX*i)
 		local z = cz+(wdZ*i)
-		if not courseplay:areaHasFruit(x, z, fruitType, 1, 1) then
+		if not courseplay:areaHasFruit(x, z, fruitType, 2, 2) then
 			if firstHit then
 				--if inversed then print("	return true, "..tostring(i)) end
 				return true,i
